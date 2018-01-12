fdbb25aff69c81c050250a2688ee200cafe8ed06
fnobakht <fnobakht@sfu.ca>
2018-01-12 05:08:47 -0800

    Handle get request for homepage

---
+++ controller/userController.js
@@ -0,0 +1,8 @@
+var User = require('../models/user');
+var Recipe = require('../models/recipe');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+exports.user_login = function(req, res, next){
+    res.render('login');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controller/userController');
+var authentication = require('./authentication');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.user_login);
 
 module.exports = router;
