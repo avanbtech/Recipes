0d70fff704a0f0a1a50c4c7ea4aaee3773ba857b
fnobakht <fnobakht@sfu.ca>
2018-01-13 13:12:28 -0800

    Handle get request for sign up

--- controller/userController.js
+++ controller/userController.js
@@ -6,3 +6,7 @@ var LocalStrategy = require('passport-local').Strategy;
 exports.user_login = function(req, res, next){
     res.render('login');
 };
+
+exports.user_signup = function(req, res, next){
+    res.render('sign-up');
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var userController = require('../controller/userController');
 var authentication = require('./authentication');
 
 /* GET users listing. */
-router.get('/', userController.user_login);
+router.get('/login', userController.user_login);
+
+router.get('/sign_up', userController.user_signup);
 
 module.exports = router;
